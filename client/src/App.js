c5fa9b03c5204c780a9a3b33379a36523f573f53
Craig Banach <craigsb93@gmail.com>
2021-11-12 10:11:41 +0000

    Delete commented out, obsolete code

--- client/src/App.js
+++ client/src/App.js
@@ -85,25 +85,6 @@ class App extends Component {
         </div>
       </Router>
     );
-
-    // if (!this.state.web3) {
-    //   return <div>Loading Web3, accounts, and contract...</div>;
-    // }
-    // return (
-    //   <div className="App">
-    //     <h1>Good to Go!</h1>
-    //     <p>Your Truffle Box is installed and ready.</p>
-    //     <h2>Smart Contract Example</h2>
-    //     <p>
-    //       If your contracts compiled and migrated successfully, below will show
-    //       a stored value of 5 (by default).
-    //     </p>
-    //     <p>
-    //       Try changing the value stored on <strong>line 42</strong> of App.js.
-    //     </p>
-    //     <div>The stored value is: {this.state.storageValue}</div>
-    //   </div>
-    // );
   }
 }
 
